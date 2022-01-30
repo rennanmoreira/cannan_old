091e6d1dccbb8d0956e42b9a7171f1f14c14930e
Rennan Moreira <rmoreira@bernhoeft.com.br>
2022-01-30 14:58:50 -0300

    chore: manifest config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    name: 'Cannan',
+    themeColor: '#525299',
+    msTileColor: '#525299',
+    manifestOptions: {
+      background_color: '#525299'
+    }
+  },
+  devServer: {
+    port: 9000,
+  },
 }
